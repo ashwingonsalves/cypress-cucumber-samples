e349b036b6173e72027b84c9b9aaf15a82bac9bd
Ashwin Gonsalves <ashwingonsalves@gmail.com>
2020-07-12 14:29:31 +0530

    Updated feature file

--- cypress/integration/cucumber-tests/login.feature
+++ cypress/integration/cucumber-tests/login.feature
@@ -1,7 +1,14 @@
 Feature: Login to Web App
 
+    As an invalid user
+    I should not be able to login
     As a valid user
     I should be able to login
+
+    Scenario: Invalid Login Credentials
+        Given I visit the login page
+        When I submit invalid login credentials
+        Then I should see failure
 
     Scenario: Valid Login Credentials
         Given I visit the login page
--- cypress/integration/cucumber-tests/login/loginSteps.js
+++ cypress/integration/cucumber-tests/login/loginSteps.js
@@ -8,8 +8,20 @@ When('I submit valid login credentials', () => {
     cy.get('#username').type('tomsmith')
     cy.get('#password').type('SuperSecretPassword!')
     cy.get('button[type="submit"]').click()
+    cy.screenshot({capture: 'fullPage'})
+})
+
+When('I submit invalid login credentials', () => {
+    cy.get('#username').type('invalid_usr')
+    cy.get('#password').type('invalid_pass')
+    cy.get('button[type="submit"]').click()
+    cy.screenshot({capture: 'fullPage'})
 })
 
 Then('I should see success', () => {
     cy.get('.flash.success').should('be.visible')
+})
+
+Then('I should see failure', () => {
+    cy.get('.flash.error').should('be.visible')
 })
